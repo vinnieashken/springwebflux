897c90cb8e352f545208b4dd6aab5fd0129c9496
Vincent <vmutune@ke.nationmedia.com>
2024-12-20 15:18:19 +0300

    fix

--- src/main/java/com/products/config/DBConfig.java
+++ src/main/java/com/products/config/DBConfig.java
@@ -3,7 +3,6 @@ package com.products.config;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
